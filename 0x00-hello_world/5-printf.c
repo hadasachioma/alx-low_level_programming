c9f03cbbc982540f843a2017e30d0d46c7fa3ded
hadasachioma <hadasachioma@gmail.com>
2023-02-20 15:45:44 -0800

    werror

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,8 +2,10 @@
 
 /**
  * main - A progam that prints a line with printf functon
+ * 
  * Return 0 (Success)
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
